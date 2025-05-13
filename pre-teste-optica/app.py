90771fc01b57c8bd01b640b2dada743ae9fbffd6
Quespy <rafaconta@yahoo.com>
2025-05-13 08:31:44 -0300

    Add files via upload

---
+++ pre-teste-optica/app.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respostas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Resposta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pergunta1 = db.Column(db.Text, nullable=False)
+    pergunta2 = db.Column(db.Text, nullable=False)
+    pergunta3 = db.Column(db.Text, nullable=False)
+    pergunta4 = db.Column(db.Text, nullable=False)
+    pergunta5 = db.Column(db.Text, nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nova_resposta = Resposta(
+            pergunta1=request.form['pergunta1'],
+            pergunta2=request.form['pergunta2'],
+            pergunta3=request.form['pergunta3'],
+            pergunta4=request.form['pergunta4'],
+            pergunta5=request.form['pergunta5']
+        )
+        db.session.add(nova_resposta)
+        db.session.commit()
+        return 'Obrigado por participar!'
+    return render_template('formulario.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
